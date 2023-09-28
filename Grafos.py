ac0700da4ef77afa58b47af157e4dc0f87284b10
jesusmanueldl <jesusmanueldl@gmail.com>
2023-09-27 23:12:54 -0600

    Se completo las funciones de pintar grafos y visualizacion de un grafo

--- Grafos.py
+++ Grafos.py
@@ -1,8 +1,10 @@
+#import pinta #archivo para pintar Grafos (debes definirla)
+
+
 #definimos un Vertice
 class Vertice:
     def __init__(self,info):
         self.inf = info
-
 
 #definimos una Arista
 class Arista:
@@ -13,23 +15,39 @@ class Arista:
 
 #definimos un Grafo
 class Grafo:
-    def __init__(self, V, A, nom):
+    def __init__(self, V, A, nombre):
         self.V = V
         self.A = A
-        self.nombre = nom
+        self.nombre = nombre
         self.inf = [list(map(lambda x: x.inf, self.V)),list(map(lambda x: x.inf, self.A)),self.nombre]
-
-
-
+        
+        
+#Definicion de los Vertices        
 v1 = Vertice(1)
 v2 = Vertice(2)
 v3 = Vertice(3)
-lv = [v1,v2,v3]
+v4 = Vertice(4)
+v5 = Vertice(5)
+#conjunto de Vertices
+lv = [v1,v2,v3,v4,v5] 
 
+#Definicion de las Aristas
 a1 = Arista(v1,v2)
-a2 = Arista(v1,v3)
-la = [a1,a2]
+a2 = Arista(v1,v5)
+a3 = Arista(v3,v4)
+a4 = Arista(v5,v1)
+a5 = Arista(v5,v2)
+a6 = Arista(v3,v2)
+a7 = Arista(v3,v1)
+a8 = Arista(v4,v5)
+#Conjunto de Aristas
+la = [a1,a2,a3,a4,a5,a6,a7,a8]
 
+#Definicion del Grafo
 g1 = Grafo(lv,la,"G1")
+print(g1.inf)
+
+#Visualizamos el Grafo, 
+#pinta.pinta_grafo(g1,"G1") #define esta funcion
 
 
